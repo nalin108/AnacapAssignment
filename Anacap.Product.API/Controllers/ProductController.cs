754de6ee9bc4f59864eac8632978a7f39c590b6d
nalin108 <nalinsharma108@gmail.com>
2020-09-19 12:58:37 +0530

    adding product resource

---
+++ Anacap.Product.API/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anacap.Product.API.Controllers
+{
+    [ApiController]
+    [Route("/[controller]")]
+    public class ProductsController: ControllerBase
+    {
+        [HttpGet(Name = nameof(GetProducts))]
+        public IActionResult GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Anacap.Product.API/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anacap.Product.API.Models
+{
+    public class Product: Resource
+    {
+        public string Name { get; set; }
+        public string Imagee { get; set; }
+        public string Type { get; set; }
+        public double Rating { get; set; }
+        public double Price { get; set; }
+        public int Users { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+}
